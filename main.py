0fda208e69965c06e5c9fe1697c85c6a7b8b33ed
Bung1e <leshkachovzhik@gmail.com>
2024-04-19 18:00:19 +0200

    bugfix

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sklearn
-
 import librosa
 import librosa.display
 import warnings
@@ -19,23 +18,22 @@ y, sr = librosa.load('E:\VS Projects\sound_classification\Data\genres_original\p
 
 audio_file, _ = librosa.effects.trim(y)
 
-
 # print('Audio File:', audio_file, '\n')
 # print('Audio File shape:', np.shape(audio_file))
 
-# zero_crossings = librosa.zero_crossings(audio_file, pad=False)
-# positions = [index for index, value in enumerate(zero_crossings) if value]
+zero_crossings = librosa.zero_crossings(audio_file, pad=False)
+positions = [index for index, value in enumerate(zero_crossings) if value]
 # print(positions)
 tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
 print(tempo)
 
-# y_harm, y_perc = librosa.effects.hpss(audio_file)
-# y_test = np.zeros_like(positions)
-# plt.figure(figsize = (16, 6))
-# plt.plot(y_harm, color = '#A300F9')
-# plt.plot(y_perc, color = '#FFB100')
-# plt.scatter(positions, y_test, color='green')
-# plt.show()
+y_harm, y_perc = librosa.effects.hpss(audio_file)
+y_test = np.zeros_like(positions)
+plt.figure(figsize = (16, 6))
+plt.plot(y_harm, color = '#A300F9')
+plt.plot(y_perc, color = '#FFB100')
+plt.scatter(positions, y_test, color='green')
+plt.show()
 
 # spectral_centroids = librosa.feature.spectral_centroid(y=audio_file, sr=sr)[0]
 
@@ -67,4 +65,3 @@ print(tempo)
 '''The spectral_rolloff value helps to separate genres, for example metal value 5000-7000, classical ~2900'''
 spectral_rolloff = librosa.feature.spectral_rolloff(y=audio_file, sr=sr)[0]
 print(np.mean(spectral_rolloff))
-
